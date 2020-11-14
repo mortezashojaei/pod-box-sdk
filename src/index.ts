f972844a1763245c1daf3297e425b6a995395120
morteza shojaei <mshojaei179@gmail.com>
2020-11-14 13:28:50 +0330

    add init module

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-// export default PodBox;
-// Object.assign(module.exports, PodBox);
+import { init } from "./init";
+
+const PodBox = {
+  init,
+};
+
+export default PodBox;
+Object.assign(module.exports, PodBox);
