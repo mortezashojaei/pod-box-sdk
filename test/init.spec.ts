35ab5972597bc4051b02d9e26fdd09ba46859a50
morteza shojaei <mshojaei179@gmail.com>
2020-11-15 13:01:13 +0330

    add test for init module

---
+++ test/init.spec.ts
@@ -0,0 +1,21 @@
+import { init } from "../src/init";
+import { constants } from "../src/__constants__";
+
+global.window = Object.create(window);
+const href = "http://example.com/";
+const search = "?first=1&second=2&third=3";
+Object.defineProperty(window, "location", {
+  value: {
+    href,
+    search,
+  },
+});
+
+describe("init works correctly", () => {
+  init();
+  it("local storage is fill", () => {
+    expect(localStorage.getItem(constants.STORAGE_DATA_KEY)).toBe(
+      '{"first":"1","second":"2","third":"3"}'
+    );
+  });
+});
