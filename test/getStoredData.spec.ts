0cb52d9468f92639ff5eba5ee21bd052693ae8db
morteza shojaei <mshojaei179@gmail.com>
2020-11-15 13:53:37 +0330

    add getStored data unit tests

---
+++ test/getStoredData.spec.ts
@@ -0,0 +1,16 @@
+import { getStoredData } from "../src/getStoredData";
+import { constants } from "../src/__constants__";
+
+describe("getStoredData module works correctly", () => {
+  it("return null when there is no data in store", () => {
+    expect(getStoredData()).toBe(null);
+  });
+  it("trow syntax error when data is not Json", () => {
+    localStorage.setItem(constants.STORAGE_DATA_KEY, "I am not Json");
+    expect(() => getStoredData()).toThrow(SyntaxError);
+  });
+  it("return object when there is a JSON on localstorage", () => {
+    localStorage.setItem(constants.STORAGE_DATA_KEY, "{'name':'test'}");
+    expect(() => getStoredData()).toThrow(SyntaxError);
+  });
+});
