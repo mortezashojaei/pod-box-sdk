ed9218f61fa4b515fc3e5a50e0482d30f191e1b4
morteza shojaei <mshojaei179@gmail.com>
2020-11-16 18:52:58 +0330

    complete get fresh token API

--- src/__constants__.ts
+++ src/__constants__.ts
@@ -1,9 +1,14 @@
 export const constants = {
-  POD_SSO_BASE_URL: "http://37.152.186.243:8000",
+  POD_SSO_BASE_URL: "https://accounts.pod.ir",
   STORAGE_DATA_KEY: "POD_BOX_DATA",
-  CLIENT_ID:"17892023x5b324db89637f1b38a2eb6de",
+  CLIENT_ID: "17892023x5b324db89637f1b38a2eb6de",
   TOKEN_KEY: "token",
   REFRESH_TOKEN_KEY: "refreshToken",
+  CODE_VERIFIER_KEY: "code_verifier",
 };
 
-export const paramTypes = [constants.TOKEN_KEY, constants.REFRESH_TOKEN_KEY];
+export const paramTypes = [
+  constants.TOKEN_KEY,
+  constants.REFRESH_TOKEN_KEY,
+  constants.CODE_VERIFIER_KEY,
+];
--- src/getFreshToken.ts
+++ src/getFreshToken.ts
@@ -1,18 +1,27 @@
 import axios from "axios";
 import { constants } from "./__constants__";
 
-export const getFreshToken = async (refresh_token: string) => {
+export const getFreshToken = async ({
+  refresh_token,
+  code_verifier,
+}: {
+  refresh_token: string;
+  code_verifier: string;
+}) => {
   try {
-    const code_verifier = generateCodeVerifier(10);
-    const code_challenge = await generateCodeChallenge(code_verifier);
     const data = await axios.post(
       `${constants.POD_SSO_BASE_URL}/oauth2/token`,
       {
         refresh_token,
+        code_verifier,
+
         grant_type: "refresh_token",
         client_id: constants.CLIENT_ID,
-        code_verifier,
-        code_challenge,
+      },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
       }
     );
     return data;
@@ -20,26 +29,3 @@ export const getFreshToken = async (refresh_token: string) => {
     throw error;
   }
 };
-
-function generateCodeVerifier(length: number) {
-  let text = "";
-  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-
-  return text;
-}
-
-async function generateCodeChallenge(codeVerifier: string) {
-  var digest = await crypto.subtle.digest(
-    "SHA-256",
-    new TextEncoder().encode(codeVerifier)
-  );
-
-  return btoa(String.fromCharCode(...new Uint8Array(digest)))
-    .replace(/=/g, "")
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_");
-}
