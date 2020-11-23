b189ffd1423afed4ea97e6902e66a8a505181691
morteza shojaei <mshojaei179@gmail.com>
2020-11-23 17:31:17 +0330

    setup babel and terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import minify from 'rollup-plugin-babel-minify';
+import { terser } from 'rollup-plugin-terser';
 import json from 'rollup-plugin-json';
 import typescript from '@rollup/plugin-typescript';
+import babel from '@rollup/plugin-babel';
 import pkg from './package.json';
 
 export default [
   {
-    input: 'src/index.js',
+    input: 'dist/index.js',
     output: {
       name: 'podBoxSDK',
       file: pkg.browser,
@@ -20,7 +21,23 @@ export default [
       commonjs({
         include: 'node_modules/**',
       }),
-      minify({ comments: false }),
+      terser({
+        ecma: '5',
+        compress: true,
+        mangle: true,
+      }),
+      babel({
+        babelHelpers: 'bundled',
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              targets:
+                '> 0.25%, last 2 versions, Firefox ESR, not dead',
+            },
+          ],
+        ],
+      }),
       json(),
     ],
   },
