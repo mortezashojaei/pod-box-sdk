74d4051efb288f1b444dc91dc1648dbad92cac91
morteza shojaei <mshojaei179@gmail.com>
2020-11-24 16:26:15 +0330

    add isTokenExpired and isLoggedIn tests

--- test/auth.spec.ts
+++ test/auth.spec.ts
@@ -1,5 +1,6 @@
 const axios = require('axios');
 import { auth } from '../src/auth';
+import { getStoredData } from '../src/store';
 import { constants } from '../src/__constants__';
 
 jest.mock('axios');
@@ -21,5 +22,41 @@ describe('isTokenExpired module works currectly', () => {
       }),
     );
     expect(auth.isTokenExpired()).toBe(false);
+  });
+});
+
+describe('setSession module works correctly', () => {
+  it('session values is in store', () => {
+    const access_token = 'token',
+      expires_in = 1,
+      refresh_token = 'refresh token';
+    auth.setSession({
+      data: { access_token, expires_in, refresh_token },
+    });
+    expect(getStoredData()[constants.TOKEN_KEY]).toBe(access_token);
+    expect(getStoredData()[constants.REFRESH_TOKEN_KEY]).toBe(
+      refresh_token,
+    );
+    expect(getStoredData()[constants.EXPIRES_IN_KEY]).toBeDefined();
+  });
+});
+
+describe('is Logged in works correctly', () => {
+  it('return false when user is not logged in', () => {
+    expect(auth.isLoggedIn()).toBe(false);
+  });
+
+  it('when there is not expired token and refresh Token => user logged in', () => {
+    //user is logged in so has token,refresh token,and token is not expired
+    localStorage.setItem(
+      constants.STORAGE_DATA_KEY,
+      JSON.stringify({
+        [constants.TOKEN_KEY]: 'token',
+        [constants.REFRESH_TOKEN_KEY]: 'refresh token',
+        [constants.EXPIRES_IN_KEY]: 1000000000000000,
+      }),
+    );
+
+    expect(auth.isLoggedIn()).toBe(true);
   });
 });
