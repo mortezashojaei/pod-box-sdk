0dbdf3d3fba171df5d316af34d13de4b8f9338a8
morteza shojaei <mshojaei179@gmail.com>
2020-11-24 11:20:43 +0330

    coplete init tests

--- test/init.spec.ts
+++ test/init.spec.ts
@@ -1,8 +1,9 @@
-import { init } from '../src/init';
+import { ConfigType, init } from '../src/init';
 import { constants } from '../src/__constants__';
 
 global.window = Object.create(window);
-const href = 'http://example.com/';
+const href = 'http://example.com/init';
+const authenticatingPage = 'http://example.com/authenticating';
 const search = '?token=1&second=2&third=3';
 Object.defineProperty(window, 'location', {
   value: {
@@ -11,11 +12,30 @@ Object.defineProperty(window, 'location', {
   },
 });
 
+const ExampleConfig: ConfigType = {
+  redirectUri: authenticatingPage,
+  scope: 'profile',
+  clientId: '18131280rd8074b3fa9fd5f074c63f334',
+};
+
 describe('init works correctly', () => {
-  init();
   it('local storage is fill', () => {
-    expect(localStorage.getItem(constants.STORAGE_DATA_KEY)).toBe(
-      '{"token":"1"}',
-    );
+    init(ExampleConfig);
+
+    const store = localStorage.getItem(constants.STORAGE_DATA_KEY);
+    expect(store).toContain(ExampleConfig.redirectUri);
+    expect(store).toContain(ExampleConfig.clientId);
+    expect(store).toContain(constants.CODE_VERIFIER_KEY);
+  });
+
+  it('local storage old values clear with init', () => {
+    const TestData = "{'testKey':'test Value'}";
+    localStorage.setItem(constants.STORAGE_DATA_KEY, TestData);
+
+    init(ExampleConfig);
+
+    expect(
+      localStorage.getItem(constants.STORAGE_DATA_KEY),
+    ).not.toContain(TestData);
   });
 });
